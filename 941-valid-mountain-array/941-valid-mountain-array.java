dab8a47ed2ed905ae4d34cfa7d96ab091edd82eb
Sanchit Kalsi <60578902+DarkSoul26@users.noreply.github.com>
2022-01-25 20:02:05 +0530

    Time: 5 ms (6.71%), Space: 51.3 MB (39.75%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.java
+++ 941-valid-mountain-array/941-valid-mountain-array.java
@@ -2,6 +2,8 @@ class Solution {
     public boolean validMountainArray(int[] a) {
         int count1 = 0;
         for(int i=1;i<a.length-1;i++){
+            if(count1>1)
+                return false;
             if(a[i]>a[i-1] && a[i]>a[i+1])
                 count1++;
             if(a[i]<a[i-1] && a[i]<a[i+1])
