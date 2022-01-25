b2a6f6bd6ce8fa7b12c2b6ed1be4fd8359734854
Sanchit Kalsi <60578902+DarkSoul26@users.noreply.github.com>
2022-01-25 20:00:14 +0530

    Time: 2 ms (56.28%), Space: 51.2 MB (43.19%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.java
+++ 941-valid-mountain-array/941-valid-mountain-array.java
@@ -1,33 +1,10 @@
 class Solution {
-    public boolean validMountainArray(int[] a) {
-        // boolean flag1 = true;
-        // boolean flag2 = true;
-        int count1 = 0, count2 = 0;
-        for(int i=1;i<a.length-1;i++){
-            if(a[i]>a[i-1] && a[i]>a[i+1])
-                count1++;
-            if(a[i]<a[i-1] && a[i]<a[i+1])
-                count2++;
-            if(a[i] == a[i-1] || a[i] == a[i+1])
-                return false;
-            // if(arr[i-1]>arr[i]){
-            //     if(!flag2)
-            //         return false;
-            //     flag1 = false;
-            // }
-            // else if(arr[i-1]<arr[i]){
-            //     if(!flag1)
-            //         return false;
-            //     flag2 = false;
-            // }
-            // else
-            //     return false;
-        }
-        if(count1 == 1){
-            if(count2 > 0)
-                return false;
-            return true;
-        }
-        return false;
+    public boolean validMountainArray(int[] arr) {
+        if(arr.length < 3) return false;
+        int l = 0;
+        int r = arr.length - 1;
+        while(l + 1 < arr.length - 1 && arr[l] < arr[l + 1]) l++;
+        while(r - 1 > 0 && arr[r] < arr[r - 1]) r--;
+        return l == r;
     }
 }
