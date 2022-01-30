2e25cf52d31b9f84ada36ce89fb21197e3d4e7af
Sanchit Kalsi <60578902+DarkSoul26@users.noreply.github.com>
2022-01-30 11:14:27 +0530

    Time: 429 ms (5.02%), Space: 92.4 MB (18.01%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -1,19 +1,102 @@
 class Solution {
-    public int largestRectangleArea(int[] heights) {
-        int n = heights.length;
-        int maxArea = 0;
-        Stack<Integer> st = new Stack<>();
+    public int largestRectangleArea(int[] a) {
+        int len = a.length;
+        int finalArea = 0;
         
-        for(int i = 0; i <= n; i++){
-            int currHeight = i == n ? 0 : heights[i];
-            while(!st.isEmpty() && currHeight < heights[st.peek()]){
-                int top = st.pop(); 
-                int width = st.isEmpty() ? i : i - st.peek() - 1;
-                int area = heights[top] * width; 
-                maxArea = Math.max(area, maxArea);
+        Stack<Integer> stk = new Stack<>();
+        Stack<Integer> stkInd = new Stack<>();
+        int[] left = new int[len];
+        int[] right = new int[len];
+        
+        // left[0] = 0;
+        // right[len-1] = len-1;
+        // stk.push(a[0]);
+        for(int i=0;i<len;i++){
+            if(!stk.isEmpty() && stk.peek()>=a[i]){
+                while(!stk.isEmpty() && stk.peek()>=a[i]){
+                    stkInd.pop();
+                    stk.pop();
+                }
+                if(stk.isEmpty())
+                    left[i] = 0;
+                else{
+                    left[i] = stkInd.peek()+1;
+                }
+                stkInd.push(i);
+                stk.push(a[i]);
             }
-            st.push(i);
+            else if(!stk.isEmpty() && stk.peek()<a[i]){
+                left[i] = stkInd.peek()+1;
+                stkInd.push(i);
+                stk.push(a[i]);
+            }
+            else{
+                left[i] = 0;
+                stkInd.push(i);
+                stk.push(a[i]);
+            }
         }
-        return maxArea;
+        
+        stkInd = new Stack<>();
+        stk = new Stack<>();
+        for(int i=len-1;i>=0;i--){
+            if(!stk.isEmpty() && stk.peek()>=a[i]){
+                while(!stk.isEmpty() && stk.peek()>=a[i]){
+                    stkInd.pop();
+                    stk.pop();
+                }
+                if(stk.isEmpty())
+                    right[i] = len-1;
+                else{
+                    right[i] = stkInd.peek()-1;
+                }
+                stkInd.push(i);
+                stk.push(a[i]);
+            }
+            else if(!stk.isEmpty() && stk.peek()<a[i]){
+                right[i] = stkInd.peek()-1;
+                stkInd.push(i);
+                stk.push(a[i]);
+            }
+            else{
+                right[i] = len-1;
+                stkInd.push(i);
+                stk.push(a[i]);
+            }
+        }
+        
+        // for(int i=0;i<len;i++){
+        //     System.out.print(left[i]+" ");
+        // }
+        // for(int i=0;i<len;i++){
+        //     System.out.print(right[i]+" ");
+        // }
+        
+        // return -1;
+        for(int i=0;i<len;i++){
+//             int lmin = 0, rmin = 0;
+//             for(lmin=i;lmin>=0;lmin--){
+//                 if(a[lmin]<a[i]){
+                    
+//                     lmin = lmin+1;
+//                     break;
+//                 }
+//             }
+//             if(lmin == -1)
+//                 lmin++;
+//             for(rmin=i;rmin<len;rmin++){
+//                 if(a[rmin]<a[i]){
+                    
+//                     rmin = rmin-1;
+//                     break;
+//                 }
+//             }
+//             if(rmin == len)
+//                 rmin--;
+            int area = (right[i]-left[i]+1)*a[i];
+            if(area>finalArea)
+                finalArea = area;
+        }
+        return finalArea;
     }
 }
