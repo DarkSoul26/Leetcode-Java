54d5cdd34fc21cec892aac8c165478440fa8c398
Sanchit Kalsi <60578902+DarkSoul26@users.noreply.github.com>
2022-02-19 18:10:50 +0530

    Time: 2 ms (79.34%), Space: 51.8 MB (18.66%) - LeetHub

---
+++ 130-surrounded-regions/130-surrounded-regions.java
@@ -0,0 +1,42 @@
+class Solution {
+    
+    public void solve(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 || i == m-1 || j == 0 || j == n-1) {
+                    if(board[i][j] == 'O')
+                        dfs(board, i, j);
+                }
+            }
+        }
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(board[i][j] == '#') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+    
+    void dfs(char[][] board, int i, int j) {
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != 'O') {
+            return;
+        }
+        board[i][j] = '#';
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
+    }
+}
