faf804b3d9aaa7ecd7ff1f2215629bda038c2703
Sanchit Kalsi <60578902+DarkSoul26@users.noreply.github.com>
2022-02-27 16:52:05 +0530

    Time: 469 ms (21.15%), Space: 52.7 MB (32.45%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -0,0 +1,53 @@
+class Solution {
+    public String longestPalindrome(String s1) {
+        StringBuffer sb1 = new StringBuffer(s1);
+        String s2 = sb1.reverse().toString();
+        int n = s1.length();
+        int dp[][] = new int[n+1][n+1];
+        for(int[] dp1: dp)
+            Arrays.fill(dp1, -1);
+        for(int i=0;i<=n;i++)
+            dp[i][0] = 0;
+        for(int i=0;i<=n;i++)
+            dp[0][i] = 0;
+        // int res = 0;
+        // int min = Integer.MAX_VALUE;
+        // int max = Integer.MIN_VALUE;
+        int prevBest = Integer.MIN_VALUE;
+        String str = "";
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                    if(dp[i][j]>prevBest){
+                        // if(i-dp[i][j]==0)
+                        //     str = s1.substring(i-dp[i][j]+1, i);
+                        // else
+                        if(isPali(s1.substring(i-dp[i][j], i))){
+                            str = s1.substring(i-dp[i][j], i);
+                            prevBest = dp[i][j];
+                        }
+                    }
+                    // if(res<dp[i][j]){
+                    //     res = dp[i][j];
+                    // }
+                }
+                else{
+                    // if(dp[i-1][j-1]>prevBest){
+                    //     str = s1.substring(i-dp[i-1][j-1]-1, i-1);
+                    //     prevBest = dp[i-1][j-1];
+                    // }
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return str;
+    }
+    
+    public boolean isPali(String s){
+        for(int i=0,j=s.length()-1;(i<=s.length()/2 && j>=s.length()/2);i++,j--)
+            if(s.charAt(i)!=s.charAt(j))
+                return false;
+        return true;
+    }
+}
