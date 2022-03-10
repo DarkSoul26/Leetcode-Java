864d939dbddd1e89ac8ee152ea9a6a9bae255574
Sanchit Kalsi <60578902+DarkSoul26@users.noreply.github.com>
2022-03-10 17:35:38 +0530

    Time: 3 ms (89.71%), Space: 54 MB (41.53%) - LeetHub

--- 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.java
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.java
@@ -1,11 +1,8 @@
 class Solution {
-
         public int maximumProduct (int[] nums) {
-
-        int n1 = Integer.MIN_VALUE, n2 = Integer.MIN_VALUE, n3 = Integer.MIN_VALUE, n4 = Integer.MAX_VALUE, n5 = Integer.MAX_VALUE;
+            int n1 = Integer.MIN_VALUE, n2 = Integer.MIN_VALUE, n3 = Integer.MIN_VALUE, n4 = Integer.MAX_VALUE, n5 = Integer.MAX_VALUE;
 
         for (int i=0; i<nums.length; i++) {
-
             if(nums[i]>n1){
                 n3 = n2;
                 n2 = n1;
@@ -18,27 +15,12 @@ class Solution {
             else if(nums[i]>n3)
                 n3 = nums[i];
             if(nums[i]<n5){
-
                 n4 = n5;
-
                 n5 = nums[i];
-
             }
             else if(nums[i]<n4)
                 n4 = nums[i];
-
         }
-
-        System.out.println(n1+" "+n2+" "+n3+" "+n4+" "+n5); 
-//         if(n2 == n3 && n2 == Integer.MIN_VALUE){
-
-//             return n1*n4*n5;
-
-//         }
-
-//         else if(n4 == Integer.MAX_VALUE)
-//             return n1*n2*n3;
-
         return Math.max(n1*n2*n3, n1*n4*n5);
         }
 }
