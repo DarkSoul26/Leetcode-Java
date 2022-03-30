d8e55c34b367ef3e660f32b6033c22bd6b34f2e8
Sanchit Kalsi <60578902+DarkSoul26@users.noreply.github.com>
2022-03-30 22:52:57 +0530

    Time: 14 ms (13.99%), Space: 42.9 MB (81.91%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -10,14 +10,5 @@ class Solution {
             dp[i] = Math.max(dp[i-1], dp[i-2]+i*a[i][1]);
         }
         return dp[10001];
-//         Map<Integer, Integer> m = new HashMap<>();
-//         for(int i=0;i<nums.length;i++)
-//             m.put(nums[i], m.getOrDefault(nums[i], 0)+1);
-//         while(true){
-//             int minEl;
-//             for(Map.Entry ml: m.entrySet()){
-                
-//             }
-//         }
     }
 }
