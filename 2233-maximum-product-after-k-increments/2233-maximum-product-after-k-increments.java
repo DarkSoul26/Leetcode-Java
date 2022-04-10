963bda777edf5427e6ac462ff7b87d464c6e9053
Sanchit Kalsi <60578902+DarkSoul26@users.noreply.github.com>
2022-04-10 10:12:14 +0530

    Time: 431 ms (50.00%), Space: 55.9 MB (50.00%) - LeetHub

--- 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.java
+++ 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.java
@@ -1,15 +1,30 @@
 class Solution {
     public int maximumProduct(int[] nums, int k) {
+        long m = 1000000007;
+        if(nums.length==1){
+            long ans=(nums[0]+k)%m;
+            return (int)ans;
+        }
         PriorityQueue<Long> pq = new PriorityQueue<>();
         for(int i=0;i<nums.length;i++)
             pq.add((long)nums[i]);
+        
         long x = -1;
-        while(k--!=0){
-            x = pq.remove();
-            pq.add(x+1);
+        // while(k--!=0){
+        //     x = pq.remove();
+        //     pq.add(x+1);
+        // }
+        while(k>0){
+            long num1=pq.remove();
+            long num2=pq.remove();
+            long diff=Math.min(k,(num2-num1)+1);
+            num1+=diff;
+            k-=diff;
+            pq.add(num1);
+            pq.add(num2);
         }
         long ans = 1;
-        long m = 1000000007;
+        
         while(!pq.isEmpty()){
             x = pq.remove();
             ans=(ans%m*x%m)%m;
