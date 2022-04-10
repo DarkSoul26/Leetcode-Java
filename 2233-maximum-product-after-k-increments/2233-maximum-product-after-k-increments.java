f153a8c4ca1af0c442a883dfc4e3aa81d3c90b91
Sanchit Kalsi <60578902+DarkSoul26@users.noreply.github.com>
2022-04-10 10:09:41 +0530

    Time: 556 ms (50.00%), Space: 129.6 MB (50.00%) - LeetHub

--- 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.java
+++ 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.java
@@ -8,57 +8,8 @@ class Solution {
             x = pq.remove();
             pq.add(x+1);
         }
-        // while(k>0){
-        //     long num1=pq.remove();
-        //     long num2=pq.remove();
-        //     long diff=Math.min(k,(num2-num1)+1);
-        //     num1+=diff;
-        //     k-=diff;
-        //     pq.add(num1);
-        //     pq.add(num2);
-        // }
-        // Arrays.sort(nums);
-        // int i = 0;
-        // // for(int j=0;j<nums.length;j++){
-        // //     System.out.print(nums[i]+" ");
-        // // }
-        // // System.out.println();
-        // while(k>0){
-        //     while(i<nums.length-1 && nums[i]==nums[i+1]){
-        //         i++;
-        //     }
-        //     if(i == nums.length-1){
-        //         // if(nums[0] == nums[nums.length-1]){
-        //         // if(nums[i-1] == nums[i]){
-        //         //     i = 0;
-        //         //     continue;
-        //         // }
-        //             nums[i]++;
-        //             k--;
-        //         // }
-        //         // else{
-        //             i = 0;
-        //             continue;
-        //         // }
-        //     }
-        //     else if(k>nums[i+1]-nums[i]){
-        //         k-=nums[i+1]-nums[i];
-        //         nums[i] = nums[i+1];
-        //         i++;
-        //     }
-        //     else{
-        //         nums[i]+=k;
-        //         k = 0;
-        //         i++;
-        //     }
-        // }
         long ans = 1;
         long m = 1000000007;
-        // for(int j=0;j<nums.length;j++){
-        //     System.out.print(nums[j]+" ");
-        //     ans=(ans%m*nums[j]%m)%m;
-        // }
-        // Iterator v = pq.iterator();
         while(!pq.isEmpty()){
             x = pq.remove();
             ans=(ans%m*x%m)%m;
