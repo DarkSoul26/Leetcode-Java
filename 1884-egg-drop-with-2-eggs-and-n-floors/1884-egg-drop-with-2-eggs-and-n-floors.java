2ae4e4608d967d96e15a084f97d7161b4dfcdc42
Sanchit Kalsi <60578902+DarkSoul26@users.noreply.github.com>
2022-04-19 12:25:32 +0530

    Time: 5 ms (63.61%), Space: 41.8 MB (27.36%) - LeetHub

---
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int dropIt(int k, int n, int dp[][]){
+        if(k == 1 || n == 0 || n == 1)
+            return dp[k][n] = n;
+        
+        if(dp[k][n]!=-1)
+            return dp[k][n];
+        int min = Integer.MAX_VALUE;
+        int i = 1, max = n;
+        // for(int i=1;i<=n;i++){
+        while(i<=max){
+            int x = -1, y = -1;
+            int mid = i+(max-i)/2;
+            if(dp[k-1][mid-1]!=-1)
+                x = dp[k-1][mid-1];
+            else 
+                x = dropIt(k-1, mid-1, dp);
+            if(dp[k][n-mid] != -1)
+                y = dp[k][n-mid];
+            else
+                y = dropIt(k, n-mid, dp);
+            
+            int t = 1+Math.max(x, y);
+            if(x<y)
+                i = mid+1;
+            else
+                max = mid-1;
+            min = Math.min(min, t);
+        }
+        return dp[k][n] = min;
+    }
+    public int twoEggDrop(int n) {
+        int dp[][] = new int[3][n+1];
+        for(int[] dp1: dp)
+            Arrays.fill(dp1, -1);
+        return dropIt(2, n, dp);
+    }
+}
