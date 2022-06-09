aac90d117a6e25ef4cd8fd3e335301c62be73d1d
Sanchit Kalsi <60578902+DarkSoul26@users.noreply.github.com>
2022-06-09 15:46:52 +0530

    Time: 410 ms (21.32%), Space: 118.9 MB (38.68%) - LeetHub

---
+++ 1711-count-good-meals/1711-count-good-meals.java
@@ -0,0 +1,20 @@
+class Solution {
+    int mod = 1000000007;
+    public int countPairs(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = arr.length;
+        long res = 0;
+        for (int num : arr) {
+            int power = 1;
+            for (int i = 0; i < 22; i++) {
+                if (map.containsKey(power - num)) {
+                    res += map.get(power - num);
+                    res %= mod;
+                }
+                power *= 2;
+            }
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        return (int) res;
+    }
+}
