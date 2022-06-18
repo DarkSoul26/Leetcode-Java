adabffeb5d2caf13e814b7f78fbfe67d4c3030a9
Sanchit Kalsi <60578902+DarkSoul26@users.noreply.github.com>
2022-06-18 23:53:51 +0530

    Time: 360 ms (80.78%), Space: 257.8 MB (35.93%) - LeetHub

---
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.java
@@ -0,0 +1,45 @@
+class TrieNode {
+    TrieNode[] children;
+    int weight;
+    public TrieNode() {
+        children = new TrieNode[27];
+        weight = 0;
+    }
+}
+
+public class WordFilter {
+    TrieNode root;
+    public WordFilter(String[] words) {
+        root = new TrieNode();
+        for (int weight = 0; weight < words.length; weight++) {
+            String word = words[weight] + "{";
+            for (int i = 0; i < word.length(); i++) {
+                TrieNode cur = root;
+                cur.weight = weight;
+                for (int j = i; j < 2 * word.length() - 1; j++) {
+                    int k = word.charAt(j % word.length()) - 'a';
+                    if (cur.children[k] == null)
+                        cur.children[k] = new TrieNode();
+                    cur = cur.children[k];
+                    cur.weight = weight;
+                }
+            }
+        }
+    }
+    public int f(String prefix, String suffix) {
+        TrieNode cur = root;
+        for (char c: (suffix + '{' + prefix).toCharArray()) {
+            if (cur.children[c - 'a'] == null) {
+                return -1;
+            }
+            cur = cur.children[c - 'a'];
+        }
+        return cur.weight;
+    }
+}
+
+/**
+ * Your WordFilter object will be instantiated and called as such:
+ * WordFilter obj = new WordFilter(words);
+ * int param_1 = obj.f(prefix,suffix);
+ */
