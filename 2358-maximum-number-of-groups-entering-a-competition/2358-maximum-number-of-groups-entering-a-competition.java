ed3f79b1890fad93c3d27c5c9e067c510190ce0b
Sanchit Kalsi <60578902+DarkSoul26@users.noreply.github.com>
2022-07-31 12:02:22 +0530

    Time: 0 ms (100.00%), Space: 70.8 MB (28.57%) - LeetHub

--- 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.java
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.java
@@ -1,23 +1,12 @@
 class Solution {
     public int maximumGroups(int[] grades) {
-        Arrays.sort(grades);
-        int i = 0, n = grades.length;
-        int maxLen = 0, sumOld = 0, grps = 0;
-        while(i<n){
-            int count = 0, sumNew = 0;
-            while(i < n && (count<=maxLen || sumOld>=sumNew)){
-                sumNew+=grades[i++];
-                count++;
-            }
-            // System.out.println(maxLen+" "+sumOld);
-            if(count>maxLen && sumOld<sumNew && i>=n)
-                return ++grps;
-            if(i>=n)
-                return grps;
-            maxLen = count;
-            sumOld = sumNew;
-            grps++;
+        int i = 1;
+        while(true){
+            if(i*(i+1)/2<=grades.length)
+                i++;
+            else
+                break;
         }
-        return grps;
+        return --i;
     }
 }
