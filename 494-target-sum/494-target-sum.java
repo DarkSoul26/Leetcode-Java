2abf5fbbaf8d111323cb1cbfcad830d5b1f04e2e
Sanchit Kalsi <60578902+DarkSoul26@users.noreply.github.com>
2022-09-02 18:55:38 +0530

    Time: 13 ms (72.20%), Space: 43.7 MB (44.25%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,20 +1,25 @@
 class Solution {
-    int count = 0;
-    public void tarIt(int[] a, int t, int i, int sum){
-        if(i >= a.length){
-            if(sum == t)
-                count++;
-            return;
-        }
-        
-        // int tempSum = sum;
-        sum+=a[i];
-        tarIt(a, t, i+1, sum);
-        sum-=2*a[i];
-        tarIt(a, t, i+1, sum);
-    }
     public int findTargetSumWays(int[] nums, int target) {
-        tarIt(nums, target, 0, 0);
-        return count;
+        
+        int sum = 0;
+        for(int i=0;i<nums.length;i++)
+            sum+=nums[i];
+        if(target>sum || sum+target<0)
+            return 0;
+        int s = (sum+target)/2;
+        if((sum+target)%2!=0)
+            return 0;
+        int[][] dp = new int[nums.length+1][s+1];
+        for(int i=0;i<nums.length+1;i++)
+            dp[i][0] = 1;
+        for(int i=1;i<nums.length+1;i++){
+            for(int j=0;j<s+1;j++){
+                if(j>=nums[i-1])
+                    dp[i][j] = dp[i-1][j]+dp[i-1][j-nums[i-1]];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[nums.length][s];
     }
 }
