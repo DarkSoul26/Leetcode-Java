c5b439594497d8fb97f22cdda3b3d7ea724e941a
Sanchit Kalsi <60578902+DarkSoul26@users.noreply.github.com>
2022-09-27 10:16:41 +0530

    Time: 2 ms (37.49%), Space: 42.3 MB (61.31%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.java
+++ 190-reverse-bits/190-reverse-bits.java
@@ -4,7 +4,7 @@ public class Solution {
         int result = 0;
         for (int i = 0; i < 32; i++) {
             result += n & 1;
-            n >>= 1;   
+            n >>>= 1;   
             if (i < 31)
                 result <<= 1;
         }
