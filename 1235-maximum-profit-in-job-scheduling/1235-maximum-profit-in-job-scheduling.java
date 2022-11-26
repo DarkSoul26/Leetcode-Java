0fe87085573e6d21ba4f7ab25d09cbfe7f3e1060
Sanchit Kalsi <60578902+DarkSoul26@users.noreply.github.com>
2022-11-26 23:11:18 +0530

    Time: 127 ms (15.55%), Space: 73.8 MB (52.19%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -10,8 +10,6 @@ class Triple{
 }
 class Solution {
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
-        
-        //to store the end time and profit associated with every index
         TreeMap<Integer, Integer> tm = new TreeMap<>();
         List<Triple> t = new ArrayList<>();
         for(int i=0;i<startTime.length;i++)
@@ -24,18 +22,7 @@ class Solution {
         });
         int max = Integer.MIN_VALUE;
         for(Triple triple: t){
-            
-            //important function associated with treemap i.e. floorkey
-            // Integer x = tm.floorKey(triple.st);
-            // if(x == null){
-            //     max = Math.max(0, max);
-            //     tm.put(triple.et, triple.prf);
-            // }
-            // else{
-            //     max = Math.max(tm.get(x)+triple.prf, max);
-            //     tm.put(triple.et, tm.get(x)+triple.prf);
-            // }
-            // max = Math.max(tm.get(triple.et), max);
+        
             Integer x = tm.floorKey(triple.st);
             Integer maxV = x == null ? 0 : tm.get(x);
             
