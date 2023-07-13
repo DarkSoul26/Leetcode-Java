6aabea01662887a18472df5de7c10cb1b609e77a
Sanchit Kalsi <60578902+DarkSoul26@users.noreply.github.com>
2023-07-13 22:17:15 +0530

    Time: 4 ms (79.65%), Space: 43.9 MB (79.71%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,56 @@
+class Solution {
+    
+    public boolean isCycle(boolean[] visited, List<List<Integer>> al, int i, int[] indegree){
+        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
+        while(!q.isEmpty()){
+            Pair p = q.poll();
+            
+            System.out.println("Key: "+p.getKey()+" Value: "+p.getValue());
+            for(int j=0;j<al.get((int)p.getKey()).size();j++){
+                if(visited[j] && j!=(int)p.getValue())
+                    return true;
+                else{
+                    visited[j] = true;
+                    q.add(new Pair(j, (int)p.getValue()));
+                }
+            }
+        }
+        return false;
+    }
+    
+    public boolean isCycleDFS(boolean[] visited, boolean dfsV[], List<List<Integer>> al, int i){
+        visited[i] = true;
+        dfsV[i] = true;
+        for(int j=0;j<al.get(i).size();j++){
+            if(!visited[al.get(i).get(j)]){
+                if(isCycleDFS(visited, dfsV, al, al.get(i).get(j)))
+                    return true;
+            }
+            else if(dfsV[al.get(i).get(j)])
+                return true;
+        }
+        dfsV[i] = false;
+        return false;
+    }
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> al = new ArrayList<>();
+        for(int i=0;i<numCourses;i++)
+            al.add(new ArrayList<>());
+        for(int i=0;i<prerequisites.length;i++){
+            if(prerequisites[i][0] == prerequisites[i][1])
+                return false;
+            List<Integer> al1 = al.get(prerequisites[i][0]);
+            al1.add(prerequisites[i][1]);
+        }
+        boolean visited[] = new boolean[numCourses];
+        boolean dfsV[]  = new boolean[numCourses];
+        for(int i=0;i<numCourses;i++){
+            if(!visited[i]){
+                if(isCycleDFS(visited, dfsV, al, i)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
