58f44a4afda92a53e68542a7db5c1c217851c290
Sanchit Kalsi <60578902+DarkSoul26@users.noreply.github.com>
2023-09-22 08:46:11 +0530

    Time: 2 ms (27.41%), Space: 42.9 MB (5.41%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        
+        if(n == 0)
+            return true;
+        if(m == 0)
+            return false;
+        int dp[][] = new int[n+1][m+1];
+        for(int[] dp1: dp)
+            Arrays.fill(dp1, -1);
+        lcs(s, t, n, m, dp);
+        if(dp[n][m]-1 == s.length())
+            return true;
+        else 
+            return false;
+        
+    }
+    
+    public int lcs(String s, String t, int n, int m, int[][] dp){
+        if(n == 0)
+            return 1;
+        if(m==0)
+            return 0;
+        
+        
+        if(dp[n][m] != -1)
+            return dp[n][m];
+        
+        if(s.charAt(n-1) == t.charAt(m-1))
+            dp[n][m] = 1+lcs(s, t, n-1, m-1, dp);
+        else
+            dp[n][m] = lcs(s, t, n, m-1, dp);
+        return dp[n][m];
+    }
+}
