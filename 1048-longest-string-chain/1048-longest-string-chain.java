a1df1e3328611adbdbf5e2638c61963994b2ace5
Sanchit Kalsi <60578902+DarkSoul26@users.noreply.github.com>
2023-09-23 09:33:57 +0530

    Time: 52 ms (30.76%), Space: 43.4 MB (68.93%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,17 +1,42 @@
 class Solution {
-     public int longestStrChain(String[] words) {
-        Map<String, Integer> dp = new HashMap<>();
-        Arrays.sort(words, (a, b)->a.length() - b.length());
-        int res = 0;
-        for (String word : words) {
-            int best = 0;
-            for (int i = 0; i < word.length(); ++i) {
-                String prev = word.substring(0, i) + word.substring(i + 1);
-                best = Math.max(best, dp.getOrDefault(prev, 0) + 1);
+    public int longestStrChain(String[] words) {
+        int n=words.length;
+        int dp[]=new int[n];
+        Arrays.fill(dp,1);
+        Arrays.sort(words,(a,b)->
+        {
+            if(a.length()<b.length())return -1;
+            else if(a.length()>b.length())return 1;
+            else return 0;
+        });
+        int max=-1;
+        for(int i=0;i<n;i++)
+        {
+            for(int prev=0;prev<=i-1;prev++)
+            {
+                if(compare(words[i],words[prev])==true && dp[prev]+1>dp[i])
+                {
+                    dp[i]=dp[prev]+1;
+                }
             }
-            dp.put(word, best);
-            res = Math.max(res, best);
+            max=(int)Math.max(max,dp[i]);
         }
-        return res;
+        return max;
+    }
+    public boolean compare(String s1,String s2)
+    {
+        if(s1.length()!=s2.length()+1)return false;
+        int first=0,second=0;
+        while(first<s1.length())
+        {
+            if(second<s2.length() && s1.charAt(first)==s2.charAt(second))
+            {
+                first+=1;
+                second+=1;
+            }
+            else first+=1;
+        }
+        if(first==s1.length() && second==s2.length())return true;
+        else return false;
     }
 }
