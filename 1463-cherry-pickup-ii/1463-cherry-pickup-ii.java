4befa35ca8325e8d482279a0668feb6e63223437
Sanchit Kalsi <60578902+DarkSoul26@users.noreply.github.com>
2024-02-11 12:04:11 +0530

    Time: 23 ms (80.35%), Space: 46.8 MB (13.47%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -1,48 +1,27 @@
 class Solution {
-    public int cherryPickup(int[][] grid) {
-        int dir[] = new int[]{-1, 0, 1};
-
-        int row = grid.length;
-        int col = grid[0].length;
-        int dp[][][] = new int[row][col][col]; 
+    Integer dp[][][];
+    int n;
+    int m;
+    public int pickIt(int[][] g, int x1, int y1, int y2){
+        if(x1 == n)
+            return 0;
+        if(x1>=n || y1>=m || y2>=m || x1<0 || y1<0 || y2<0)
+            return 0;
+        if(dp[x1][y1][y2] != null)
+            return dp[x1][y1][y2];
+    
+        int max = Integer.MIN_VALUE;
+        for(int m1=-1; m1<=1; m1++)
+            for(int m2=-1; m2<=1; m2++)
+                max = Math.max(max, pickIt(g, x1+1, y1+m1, y2+m2));
+        int c = y1 == y2?g[x1][y1]:g[x1][y1]+g[x1][y2];
+        return dp[x1][y1][y2] = c+max;
         
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
-                for(int k = 0; k < col; k++){
-                    dp[i][j][k] = -1;
-                }
-            }
-        }
-        int col1 = 0; 
-        int col2 = col - 1; 
-        
-        dp[0][col1][col2] = grid[0][col1] + grid[0][col2]; 
-        int max = dp[0][col1][col2]; 
-        
-        for(int i = 1; i < row; i++){ 
-            for(int c1 = 0; c1 < col; c1++){ 
-                for(int c2 = 0; c2 < col; c2++){
-                    int prev = dp[i - 1][c1][c2]; 
-                    if(prev >= 0){ 
-                        for(int d1: dir){ 
-                            col1 = d1 + c1;
-                            for(int d2: dir){ 
-                                col2 = d2 + c2;
-                                if(inRange(col1, col) && inRange(col2, col)){
-                                    dp[i][col1][col2] = Math.max(dp[i][col1][col2], prev+(col1 == col2 ? grid[i][col1] : (grid[i][col1] + grid[i][col2]))); 
-                                    max = Math.max(max, dp[i][col1][col2]); 
-                                }
-                            }
-                        }
-                    }
-                    
-                }
-            }
-        }
-        return max;
     }
-
-    public boolean inRange(int val, int limit){
-        return 0 <= val && val < limit;
+    public int cherryPickup(int[][] grid) {
+        n = grid.length;
+        m = grid[0].length;
+        dp = new Integer[n][m][m];
+        return pickIt(grid, 0, 0, m-1);
     }
 }
