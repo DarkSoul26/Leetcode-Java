31b2a0b856a814eccf4432e062599f51ee06533b
Sanchit Kalsi <60578902+DarkSoul26@users.noreply.github.com>
2024-12-15 13:31:26 +0530

    Time: 134 ms (81.13%), Space: 41.8 MB (37.03%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,36 +1,41 @@
 class Solution {
-    public boolean getTheWord(char[][] board, String word, int n, int m, int i, int j, int len, boolean[][] isVisited){
-        if(len>=word.length())
-            return true;
-        if(board[i][j] != word.charAt(len) || isVisited[i][j])
-            return false;
-        isVisited[i][j] = true;
-        if(i<n-1)
-            if(getTheWord(board, word, n, m, i+1, j, len+1, isVisited))
-                return true;
-        if(j<m-1)
-            if(getTheWord(board, word, n, m, i, j+1, len+1, isVisited))
-                return true;
-        if(i>0)
-            if(getTheWord(board, word, n, m, i-1, j, len+1, isVisited))
-                return true;
-        if(j>0)
-            if(getTheWord(board, word, n, m, i, j-1, len+1, isVisited))
-                return true;
-        isVisited[i][j] = false;
-        return false;
-    }
     public boolean exist(char[][] board, String word) {
-        boolean[][] isVisited= new boolean[board.length][board[0].length];
-        if(board.length == 1 && board[0].length == 1)
-            if(board[0][0] == word.charAt(0) && word.length() == 1)
-                return true;
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                if(getTheWord(board, word, board.length, board[0].length, i, j, 0, isVisited))
-                    return true;
+        int n = board.length;
+        int m = board[0].length;
+        boolean[][] visit = new boolean[n][m];
+        char a = word.charAt(0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j] == a){
+                    if(dfs(i,j,0,visit,board,word))
+                        return true;
+                    // System.out.println();
+                }
             }
         }
         return false;
+    }
+    
+    
+    public boolean dfs(int i, int j, int k, boolean[][] visit, char[][] board, String word){
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length)
+            return false;
+        if(visit[i][j])
+            return false;
+        visit[i][j] = true;
+        boolean ans = false;
+        if(word.charAt(k) == board[i][j]){
+            
+            // System.out.print(word.charAt(k)+" "+i+" "+j);
+            if(k == word.length()-1)
+                return true;
+            ans = dfs(i+1, j, k+1, visit, board, word) ||
+                dfs(i, j+1, k+1, visit, board, word) ||
+                dfs(i-1, j, k+1, visit, board, word) ||
+                dfs(i, j-1, k+1, visit, board, word);
+        }
+        visit[i][j] = false;
+        
+        return ans;
     }
 }
